2b766e82f977dce499eda1604c5e4daae9f6e079
Allekz <72378963+Allekz@users.noreply.github.com>
2021-10-28 13:41:14 +0200

    Add files via upload

--- Entorn VGI-MFC-VS2019 - GL3.3+/Estructura-de-datos/Board.h
+++ Entorn VGI-MFC-VS2019 - GL3.3+/Estructura-de-datos/Board.h
@@ -7,6 +7,8 @@
 #define MIN_X 0
 #define MAX_X 1
 
+#define MAX_BLOCK 36
+
 // TODO: altura máxima del taulell en pisos
 #define MAX_HEIGHT 10
 
@@ -14,7 +16,7 @@ class Board
 {
 private:
     /* data */
-    list<Block> m_blocks;
+    list<list<Block>> m_blocks;
     int m_height;
 public:
     Board();
@@ -34,9 +36,29 @@ Board::~Board()
 }
 
 void Board::checkFloors(){
-    int completedFloors = 0;
-    bool notComplete = false;
-    for(int h = 0; h < m_height; h++){
+    list<list<Block>>::iterator it = m_blocks.begin();
 
+    for(it; it != m_blocks.end(); it++){
+        if(it.length() == MAX_BLOCK){
+            // DESTROY pis
+            im_blocks[it].delete();
+
+            // Bucle per baixar les peces de sobre
+
+            // Creem un iterador auxiliar que anirá desde el pis on hem esborrat fins al pis mes alt
+            list<Block>::iterator it_aux = it;
+            // Per cada bloc dins cada pis decrementem la seva Y
+            for(it_aux; it_aux != m_blocks.end(); it_aux++){ // Pis
+                list<Block>::iterator it_block = m_blocks[it_aux].begin();
+                for(it_block; it_block != it_block.end(); it++){ // Bloc
+                    it_block->setPosition(it_block->getPosX(), it_block->getPosY() - BLOCK_SIZE, it_block->getPosZ());
+                }
+            }
+            
+        }
     }
 }
+
+// TODO:
+    // Funcio (càmera): fa falta un getHeight()
+    // 
