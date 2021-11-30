a753208f1309c605382eeb5de9ef6b1a57c984dc
marti1999 <mcaixal1999@gmail.com>
2021-11-30 23:34:50 +0100

    fixing bug where left rotation would not turn back to original position

--- Entorn VGI-MFC-VS2019 - GL3.3+/EntornVGI/Estructura-de-datos/Piece.h
+++ Entorn VGI-MFC-VS2019 - GL3.3+/EntornVGI/Estructura-de-datos/Piece.h
@@ -284,7 +284,7 @@ bool Piece::rotateLeftOverY(vector<vector<vector<Block>>>& blocksTaulell) {
 			return true;
 		}
 	}
-	rotateLeftOverY(blocksTaulell);
+	rotateRightOverY(blocksTaulell);
 	return false;
 }
 
